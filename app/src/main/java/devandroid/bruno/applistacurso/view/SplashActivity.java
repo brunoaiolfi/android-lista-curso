be1e4d55061c413e94a9d1b7671c8d103c915ebf
Bruno Aiolfi <bruno.aiolfi154@gmail.com.br>
2024-04-26 16:28:58 -0300

    adicionando tela splash

---
+++ app/src/main/java/devandroid/bruno/applistacurso/view/SplashActivity.java
@@ -0,0 +1,34 @@
+package devandroid.bruno.applistacurso.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import devandroid.bruno.applistacurso.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static final int TIME_OUT_SPLASH = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        cumutarTelaSplash();
+    }
+
+    private void cumutarTelaSplash() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent telaPrincipal = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(telaPrincipal);
+
+                finish();
+            }
+        }, TIME_OUT_SPLASH);
+    }
+}
